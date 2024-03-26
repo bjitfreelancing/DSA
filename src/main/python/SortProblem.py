2f9bacb260c721edb897c95157a771d22676be69
bjitfreelancing <bjitfreelancing@gmail.com>
2024-03-26 22:27:48 +0530

    Python soorting

---
+++ src/main/python/SortProblem.py
@@ -0,0 +1,22 @@
+print("Hello world !")
+
+class sort:
+    def __init__(self) -> None:
+        pass
+
+    def sort_problem1(self):
+        arr = [5,9,1,3,3]
+        print(f"printed array: {arr} in sort_problem1")
+        n = len(arr)
+        for i in range(0, n-1):
+            for j in range(i+1, n-1):
+                if (arr[i] + arr[j] == 6):
+                    return True
+        return False
+
+if __name__ == "__main__":
+    sort = sort()
+    flag = sort.sort_problem1()
+    print(f"Return value from function: {flag}")
+
+        
